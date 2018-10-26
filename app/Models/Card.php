7a62a8132d3f56457570d451982a67d341503c05
Unknown <felix.sabatie@gmail.com>
2018-10-26 19:04:02 -0400

    Move models to own package

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Mob extends Model
+class Card extends Model
 {
     //
 }
--- app/Models/Mob.php
+++ app/Models/Mob.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Card extends Model
+class Mob extends Model
 {
     //
 }
