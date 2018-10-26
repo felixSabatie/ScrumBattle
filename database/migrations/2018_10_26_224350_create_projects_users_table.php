87414dbb895a4af7c65add03f62d22730878af55
Unknown <felix.sabatie@gmail.com>
2018-10-26 18:55:42 -0400

    Create projects_users table

---
+++ database/migrations/2018_10_26_224350_create_projects_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects_users', function(Blueprint $table) {
+            $table->dropForeign(['project_id', 'user_id']);
+        });
+        Schema::dropIfExists('projects_users');
+    }
+}
