439021ad62037e06e26d048cf6d8793978bf6596
Fabien <fabien.ogli@etu.univ-lyon1.fr>
2018-10-26 18:53:07 -0400

    Create Card

---
+++ database/migrations/2018_10_26_224752_create_cards_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreign('column_id')
+                ->references('id')
+                ->on('columns');
+            $table->string("name");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
