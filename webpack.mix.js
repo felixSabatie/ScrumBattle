1a86711570fde08c69bffa1bdfba4a4dd6d45f94
Irindul <mathieu.regnard2@gmail.com>
2018-10-27 00:10:15 -0400

    removed fucking browser-sync

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,4 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/bootstrap.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .version()
-    .browserSync('localhost:8000');
+    .version();
