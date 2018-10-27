e1631d07c173ea44be47eab220a5e351be9763f6
Unknown <felix.sabatie@gmail.com>
2018-10-26 22:24:56 -0400

    Laravel auth generated and working

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Http\Request;
 */
 
 Route::group(['middleware' => ['api']], function() {
-    Route::middleware('auth:api')->get('/user', function (Request $request) {
+    Route::get('/user', function (Request $request) {
         return $request->user();
     });
 
