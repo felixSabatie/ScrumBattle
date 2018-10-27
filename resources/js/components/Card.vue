22620d9c60cec30a4408251074128fc8f305df97
Irindul <mathieu.regnard2@gmail.com>
2018-10-27 12:25:25 -0400

    Modal added

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card">
+  <div class="card" @click="openModal">
     <div class="content">
       {{card.name}}
     </div>
@@ -11,24 +11,55 @@
 
     <div class="users">
       <div class="user" v-for="user in card.users"> 
-        <img class="user-image" src="https://picsum.photos/20/20" :alt="user.name">
+        <img class="user-image" :src="user.avatar" :alt="user.name">
       </div>
     </div>
-      
+
+    <modal v-if="showModal" @close="closeModal">
+      <div slot="header">
+        <h1>{{card.name}}</h1>
+      </div>
+
+      <div slot="body">
+        <div v-for="user in card.users" class="modal-users">
+          <div class="modal-users-info">
+            {{user.name}}
+          </div>
+         
+        </div>
+      </div>  
+    </modal>
+    
   </div>
 </template>
 
 <script>
+import Modal from "./Modal";
 export default {
   name: "Card",
+  components: {
+    Modal
+  },
   props: {
     card: {
       type: Object
     }
   },
+  data() {
+    return {
+      showModal: false,
+      toRemove: [],
+    }
+  },
   methods: {
     onRemoveClicked() {
-      this.$emit('remove-card', this.card);
+      this.$emit("remove-card", this.card);
+    },
+    openModal() {
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
     }
   }
 };
@@ -58,12 +89,17 @@ export default {
 
   .users {
     .user {
-      
       .user-image {
         border-radius: 50%;
         height: 20px;
         width: 20px;
       }
+    }
+  }
+
+  .modal-header {
+    h1 {
+      font-size: 18px;
     }
   }
 }
