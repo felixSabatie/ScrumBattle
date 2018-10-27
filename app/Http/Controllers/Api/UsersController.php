e498473112ebe6129a93dada586d73c6f2c4375b
Unknown <felix.sabatie@gmail.com>
2018-10-27 14:34:46 -0400

    Route get users done

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class UsersController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(User::all());
+    }
 }
