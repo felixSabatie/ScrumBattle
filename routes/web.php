411f45e63e03ba11492d13703da94b6a2bcb7e0f
Unknown <felix.sabatie@gmail.com>
2018-10-27 10:26:58 -0400

    Set vuejs routes in last not to interfere with the rest

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-Route::get('/{any?}', function () {
-    return view('welcome');
-})->where('any', '.*');
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/{any?}', function () {
+    return view('welcome');
+})->where('any', '.*');
