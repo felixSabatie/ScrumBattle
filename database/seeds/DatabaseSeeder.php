d4be5f66f6e72378ef88588878c6e42dfbb263d1
Unknown <felix.sabatie@gmail.com>
2018-10-27 23:30:47 -0400

    Oopsie forgot to uncomment

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//         $this->call(MobsSeeder::class);
+         $this->call(MobsSeeder::class);
          $this->call(OAuthClientsSeeder::class);
     }
 }
